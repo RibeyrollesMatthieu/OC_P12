73bb4efb8c662aa3b47aa8f0494c06beca860373
Matthieu Ribeyrolles <ribeyrolles.matthieu@protonmail.com>
2024-05-03 14:07:07 +0200

    updated nextui version

--- react-update/src/components/forms/CreateEmployee.tsx
+++ react-update/src/components/forms/CreateEmployee.tsx
@@ -109,6 +109,7 @@ export const CreateEmployeeForm = () => {
         value={birthdate}
         onChange={setBirthdate}
       />
+
       <DatePicker
         label='Start date'
         labelPlacement='outside'
